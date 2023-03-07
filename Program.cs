cfb27cee189b4d045ddbe072d60597ad5a59ad5f
Ali-95705Al <120209968+Ali-95705Al@users.noreply.github.com>
2023-03-07 09:23:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,31 @@
-﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, 
-//сколько чисел больше 0 ввёл пользователь.
+﻿ //Напишите программу, которая найдёт точку пересечения двух прямых,
+ // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-//0, 7, 8, -2, -2 -> 2
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-//1, -7, 567, 89, 223-> 3
+double b1 = Promt ($"Введите константу b1: ");
+double k1 = Promt ($"Введите коэффицент k1: ");
+double b2 = Promt ($"Введите константу b2: ");
+double k2 = Promt ($"Введите коэффицент k2: ");
+(double x,double y) = FindXY (b1,b2,k1,k2);
 
-Console.Write("Введите количество элементов массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] Array = new int[m];
+Console.WriteLine($"Координаты точки песечения = ({x:F3};{y:F3})");
 
-void mas(int m)
+int Promt (string message) // метод считывание с консоли 
 {
-for (int i = 0; i < m; i++)
+    Console.Write(message);
+    int value;
+    if (int.TryParse(Console.ReadLine(),out value)) //проверка условия корректности ввода
+    {
+        return value;
+    }
+     Console.WriteLine("Вы ввели не число");
+     Environment.Exit(1);
+     return 0;
+}
+(double,double) FindXY (double b1,double b2,double k1,double k2)
 {
-Console.WriteLine($"Введите {i+1} элемент массива ");
-Array[i] = Convert.ToInt32(Console.ReadLine());
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * x + b1);
+    return (x,y);
 }
-
-}
-
-int kol(int[] Array)
-{
-int i=0;
-int sum = 0;
-while (i < Array.Length)
-{
-if(Array[i]>0)
-sum = sum + 1;
-i = i + 1;
-}
-return sum;
-}
-
-mas(m);
-Console.Write($"\n Чисел больше нуля: {kol(Array)}");
